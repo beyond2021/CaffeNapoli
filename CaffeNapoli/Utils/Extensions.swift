f0893a03029f50f8ca7268b15eac8a4f3a0221a2
Keevin Mitchell <keevin.mitchell@gmail.com>
2018-06-12 06:20:31 -0400

    Missing github files UserProfile folder commit

--- CaffeNapoli/Utils/Extensions.swift
+++ CaffeNapoli/Utils/Extensions.swift
@@ -169,3 +169,29 @@ extension UIView {
         
     }
 }
+extension UIAlertController {
+    
+    /// Initialize an alert view titled "Oops" with `message` and single "OK" action with no handler
+    convenience init(message: String?) {
+        self.init(title: "Oops", message: message, preferredStyle: .alert)
+        
+        let dismissAction = UIAlertAction(title: "OK", style: .default)
+        addAction(dismissAction)
+        
+        preferredAction = dismissAction
+    }
+    
+    /// Initialize an alert view titled "Oops" with `message` and "Retry" / "Skip" actions
+    convenience init(message: String?, retryHandler: @escaping (UIAlertAction) -> Void) {
+        self.init(title: "Oops", message: message, preferredStyle: .alert)
+        
+        let retryAction = UIAlertAction(title: "Retry", style: .default, handler: retryHandler)
+        addAction(retryAction)
+        
+        let skipAction = UIAlertAction(title: "Skip", style: .default)
+        addAction(skipAction)
+        
+        preferredAction = skipAction
+    }
+    
+}
